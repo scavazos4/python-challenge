ddf51eed34a27cb3071b53389a4a1f44d976e0bb
Shawn Cavazos <scavazos4@gmail.com>
2021-04-05 15:27:59 -0500

    note

--- PyBank/main.py
+++ PyBank/main.py
@@ -2,40 +2,39 @@ import os
 import csv
 
 # Path to collect data from the Resources folder
-PyBankCsv = os.path.join('Resources', 'budget_data')
+PyBankCsv = os.path.join('Resources', 'budget_data.csv')
 
+dates = []
+revenues = []
 
-
-# Define the function and have it accept the 'budget_data' as its sole parameter
-def printData(BankData)
-    date = str(BankData[0])
-    profit = float(BankData[1])
-
-    totalMonths = len(list(date))
-
-    totalProfit = sum(list(profit))
-
-    averageChange = totalProfit / totalMonths
-
-    profitTracker1 = 0
-    profitTracker2 = 0
-    lossTracker1 = 0
-    lossTracker2 = 0
-
+averageRevenue = 0
+with open(PyBankCsv) as csvfile:
+    csvReader = csv.reader(csvfile, delimiter=',')
+    next(csvfile)
     for row in csvReader:
-        for i in profit:
-                profitTracker1 = profit[i+1] - profit[i]
-                if profitTracker1 >= profitTracker2:
-                    profitTracker2 = profit[i+1] - profit[i]
-                    dateTrackerProfit = date[i+1]
+        dates.append(row[0])
+        revenues.append(int(row[1]))
+       
+totalMonths = len(dates)
+totalProfit = sum(revenues)
+
+changeTotal = 0
+for i in revenues:
+    change = (i) - (i+1)
+    changeTotal += change
+
+AverageRevenueChange = changeTotal / totalMonths
+
+print("Financial Analysis")
+print("----------------------------")
+print(f"Total Months: {totalMonths}")
+print(f"Total Revenue: {totalProfit}")
+print(f"Average Revenue Change: {averageRevenue}")
+#    print(f"Greatest Increase in Profit: {dateTrackerProfit} $ {profitTracker2}" )
+#    print(f"Greatest Decrease in Profit: {dateTrackerLoss} $ {lossTracker2}" )
 
 
-    for row in csvReader:
-        for i in profit:
-            lossTracker1 = profit[i+1]-profit[i]
-            if losstracker1 <= lossTracker2
-                lossTracker2 = profit[i+1] - profit[i]
-                dateTrackerLoss = date[i+1]
+
 
 
 
